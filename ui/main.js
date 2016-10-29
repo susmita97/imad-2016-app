414e18f1b2fa24d498b896688eb94a0191fe99df
susmita97 <skaryakarte23@gmail.com>
2016-10-29 13:12:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,3 @@ var img=document.getElementById("madi");
 img.onclick = function () {
     img.style.marginRight = '200px';
 };
-var img=document.getElementById("madi");
-img.onclick = function () {
-    img.style.marginLeft = '400px';
-};
