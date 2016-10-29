f935a74aabce282bbd4c72b5487718e719c79158
susmita97 <skaryakarte23@gmail.com>
2016-10-29 13:21:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,10 @@
 console.log('Loaded!');
 var img=document.getElementById("madi");
+var marginLeft = 0;
+function moveRight (){
+    marginLeft = marginLeft =10;
+    img.style.marginLeft = marginLeft = 'px';
+}
 img.onclick = function () {
-    var interval = setInterval(moveRight,200);
+    var interval = setInterval(moveRight,100);
 };
